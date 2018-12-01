9fbbdf2acc187658414d86d607989d29cfe01c33
kidachi_p <1376106928@qq.com>
2018-12-01 21:13:24 +0800

    maple: initial tree push
    Change-Id: I06c097ac4106d6d92e71b98b87065c9f4d4a6324

---
+++ README.md
@@ -0,0 +1,28 @@
+Copyright (C) 2019 - The LineageOS Project
+
+Device configuration for Sony Xperia XZ Premium
+==============
+
+The Xperia XZ Premium (codenamed _"maple"_) is a premium-range phone from Sony. The single SIM variant has a model number of G8141, while the dual SIM variant G8142.
+
+Sony Xperia XZ Premium was announced in February 2017.
+
+## Device specifications
+
+| Device       | Sony Xperia XZ Premium                             |
+| -----------: | :------------------------------------------------- |
+| SoC          | Qualcomm MSM8998 Snapdragon 835                    |
+| CPU          | Quad-core 2.45GHz Kryo & quad-core 1.9GHz Kryo     |
+| GPU          | 710MHz Adreno 540                                  |
+| Memory       | 4GM RAM (LPDDR4X)                                  |
+| Shipped Android version | 7.1.1                                   |
+| Storage      | 64GB (UFS 2.1 2-LANE Flash)                        |
+| Battery      | Non-removable Li-Po 3230 mAh                       |
+| Dimensions   | 156 x 77 x 7.9 mm                                  |
+| Display      | 3840 x 2160 px, 5.5 inches (801 PPI), supports HDR |
+| Rear camera  | 19 MP, 1/2.3 memory-stacked Exmor, 25mm, f/2.0, BIONZ, RGBC-IR, 960fps video recording |
+| Front camera | 13 MP, 1/3.06 Exmor RS, 22mm, f/2.00               |
+
+## Device picture
+
+![XZ Premium](https://www.kickmobiles.com/images/thumbs/0007984_sony-xperia-xz-premium-g8141-64gb-deepsea-black_610.jpeg "XZ Premium")
---
+++ device.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit from yoshino (Sony's msm8998 platform)
+$(call inherit-product, device/sony/yoshino/common.mk)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# HWUI from Stock
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=4096
+
+# Dalvik from Stock
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
+
+# Call the proprietary setup
+$(call inherit-product, vendor/sony/maple/maple-vendor.mk)
